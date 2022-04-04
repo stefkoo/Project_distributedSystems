6759d08a9c40c8de9e184dce01bfaad631092fd9
sttait00 <sttait00@hs-esslingen.de>
2022-04-04 16:31:45 +0200

    Initial commit

---
+++ demo/src/main/java/de/hs/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package de.hs.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class Hello {
+	@RequestMapping("/hello")
+	String greeting(){
+		return "Hello Esslingen SS22";
+	}
+
+
+
+	@GetMapping("/employees/{id}")
+	@ResponseBody
+	public String getEmployeesById(@PathVariable String id) {
+		return "ID: " + id;
+	}
+}
+
